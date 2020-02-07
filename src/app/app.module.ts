d3409f6b4f42605bc45f88ddc4ae897c6cd4c906
Alex Hart <alex@cultureiq.com>
2020-02-07 15:30:29 -0500

    early changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,7 @@ import { AlertComponent } from './_components';
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
         // provider used to create fake backend
-        fakeBackendProvider
+        // fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
