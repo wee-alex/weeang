ebff5432a680a90bf421bab40157dda124cc3307
Alex Hart <alex@cultureiq.com>
2020-02-15 15:43:13 -0500

    some more

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -20,7 +20,7 @@ export class AuthenticationService {
     }
 
     login(email, password) {
-        return this.http.post<any>(`${config.apiUrl}/api/authenticate`, { email, password })
+        return this.http.post<any>(`${config.apiUrl}/api/login_submit`, { email, password })
             .pipe(map(user => {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
